49b34744334d7a793f542cc088d2053d1dd50909
Chau Truong <chauminh.0210@gmail.com>
2024-04-26 02:01:13 -0500

    reformat

--- src/python/Helper/new_types.py
+++ src/python/Helper/new_types.py
@@ -1,7 +1,16 @@
 from typing import List, Union
 
 class CourseShell:
-  def __init__(self, uid_: str, code_: str, subject_: str, number_: str):
+  """
+  CouseShell contains basic information of a course.
+  """
+  def __init__(
+      self,
+      uid_: str,
+      code_: str,
+      subject_: str,
+      number_: str
+    ):
     self.uid = uid_
     self.code = code_
     self.subject = subject_
@@ -9,13 +18,16 @@ class CourseShell:
 
 class PrereqCourses:
   """
-  This class is going to be a union of these type formats:
-  CourseShell,
-  List[CourseShell],
-  { 'and' : List[CourseShell] },
-  { 'or' : List[CourseShell] }
+  This class is going to be a union of these type formats:\n
+  - CourseShell,
+  - List[CourseShell],
+  - { 'and' : List[CourseShell] },
+  - { 'or' : List[CourseShell] }
   """
-  def __init__(self, courses_: Union[List['PrereqCourses'], dict, CourseShell]):
+  def __init__(
+      self,
+      courses_: Union[List['PrereqCourses'], dict, CourseShell]
+    ):
     self = courses_
 
 class PrereqList:
@@ -24,12 +36,20 @@ class PrereqList:
 
 class Course(CourseShell):
   """
-  Course type contains all information about a course
-  It includes CourseShell with additional fields
+  Course contains all information about a course.\n
+  It includes CourseShell with additional fields.
   """
   def __init__(
-      self, uid_: str, code_: str, subject_: str, number_: str,
-      name_: str, longname_: str, info_: str, prereq_: PrereqList):
+      self,
+      uid_: str,
+      code_: str,
+      subject_: str,
+      number_: str,
+      name_: str,
+      longname_: str,
+      info_: str,
+      prereq_: PrereqList
+    ):
     # Initialize CourseShell
     super().__init__(uid_, code_, subject_, number_)
 
