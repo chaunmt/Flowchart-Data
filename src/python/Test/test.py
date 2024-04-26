66dc96ec3b884503c4cbf3188b800e3e9919b8c8
Chau Truong <chauminh.0210@gmail.com>
2024-04-26 03:07:12 -0500

    reformat and add more cases

--- src/python/Test/test.py
+++ src/python/Test/test.py
@@ -15,6 +15,16 @@ from Filter.string_filter import *
 from Filter.course_filter import *
 from Filter.prereq_filter import *
 
+def merge_lists(list1, list2):
+  """
+  Merge members at the same index of 2 lists together.
+  """
+  merged_list = []
+  for first, second in zip(list1, list2):
+    merged_list.append({**first, **second})
+
+  return merged_list
+
 def assert_eq(result, goal):
   """
   Assert whether result equals goal.\n
@@ -22,31 +32,9 @@ def assert_eq(result, goal):
   """
   assert result == goal, f"Unwanted result: {result}"
 
-class TestNewTypes(unittest.TestCase):
+class TestString(unittest.TestCase):
   """
-  Test module new_types
-  """
-  def setUp(self):
-    pass
-  
-  def tearDown(self):
-    pass
-
-  def test_course_shell(self):
-    pass
-
-  def test_course(self):
-    pass
-
-  def test_prereq_courses(self):
-    pass
-
-  def test_prereq_list(self):
-    pass
-
-class TestStringSplitter(unittest.TestCase):
-  """
-  Test module string_splitter
+  Parent class to set up and tear down all child classes.
   """
   def setUp(self):
     self.s1 = "Hello, world!"
@@ -55,10 +43,110 @@ class TestStringSplitter(unittest.TestCase):
     self.s4 = "C?S1!0^1@"
     self.s5 = "This ! is ? a ^ test."
     self.s6 = "!@#$%^&*()"
-  
+    self.s7 = ""
+    self.s8 = "   1   "
+    
   def tearDown(self):
     pass
 
+class TestCourse(unittest.TestCase):
+  """
+  Parent class to set up and tear down all child classes.
+  """
+  def setUp(self):
+    self.course_shell_data = [
+      {
+        'uid_': '1',
+        'code_': 'CSC101',
+        'subject_': 'Computer Science',
+        'number_': '101',
+      },
+      {
+        'uid_': '2',
+        'code_': 'CSC101',
+        'subject_': 'Computer Science',
+        'number_': '101',
+      },
+      {
+        'uid_': '3',
+        'code_': 'CSC102',
+        'subject_': 'Computer Science',
+        'number_': '102',
+      }
+    ]
+
+    self.additional_data = [
+      {
+        'name_': 'Introduction to Computer Science',
+        'longname_': 'CS 101',
+        'info_': 'Introductory course covering basic concepts in computer science.',
+        'prereq_': None
+      },
+      {
+        'name_': 'Introduction to Computer Science',
+        'longname_': 'CS 101',
+        'info_': 'Introductory course covering basic concepts in computer science.',
+        'prereq_': None
+      },
+      {
+        'name_': 'Data Structures',
+        'longname_': 'CS 102',
+        'info_': 'A course focusing on data structures and algorithms.',
+        'prereq_': None
+      }
+    ]
+
+    # Merge course_shell_data with additional attributes for each course
+    self.course_data = merge_lists(self.course_shell_data, self.additional_data)
+
+    # Initialize CourseShell objects
+    self.course_shells = [CourseShell(**data) for data in self.course_shell_data]
+
+    # Initialize Course objects
+    self.courses = [Course(**data) for data in self.course_data]
+
+  def tearDown(self):
+    pass
+
+class TestFilter(unittest.TestCase):
+  """
+  Test module filter.
+  """
+  # Class Filter is tested with its children classes
+  pass
+
+class TestNewTypes(TestCourse, TestString):
+  """
+  Test module new_types
+  """
+  def test_course_shell(self):
+    for index, shell in enumerate(self.course_shells):
+      assert_eq(shell.uid, self.course_shell_data[index]['uid_'])
+      assert_eq(shell.code, self.course_shell_data[index]['code_'])
+      assert_eq(shell.subject, self.course_shell_data[index]['subject_'])
+      assert_eq(shell.number, self.course_shell_data[index]['number_'])
+
+  def test_course(self):
+    for index, course in enumerate(self.courses):
+      assert_eq(course.uid, self.course_data[index]['uid_'])
+      assert_eq(course.code, self.course_data[index]['code_'])
+      assert_eq(course.subject, self.course_data[index]['subject_'])
+      assert_eq(course.number, self.course_data[index]['number_'])
+      assert_eq(course.name, self.course_data[index]['name_'])
+      assert_eq(course.longname, self.course_data[index]['longname_'])
+      assert_eq(course.info, self.course_data[index]['info_'])
+      assert_eq(course.prereq, self.course_data[index]['prereq_'])
+
+  def test_prereq_courses(self):
+    pass
+
+  def test_prereq_list(self):
+    pass
+  
+class TestStringSplitter(TestString):
+  """
+  Test module string_splitter.
+  """
   def test_at_index(self):
     assert_eq(StringSplitter.at_index(self.s1, 5), ['Hello,', ' world!'])
     assert_eq(StringSplitter.at_index(self.s1, 7), ['Hello, w', 'orld!'])
@@ -164,114 +252,58 @@ class TestStringSplitter(unittest.TestCase):
     """Need to change type name"""
     pass
 
-class TestStringChecker(unittest.TestCase):
+class TestStringChecker(TestString):
   """
-  Test module string_checker
+  Test module string_checker.
   """
-  def setUp(self):
-    self.s1 = "Hello World"
-    self.s2 = "12345"
-    self.s3 = "abc   123"
-    self.s4 = "Testing 1 2 3"
-    self.s5 = "NoNumbersHere!"
-    self.s6 = ""
-    self.s7 = "123abc456"
-
-  def tearDown(self):
-    pass
-
   def test_has_number(self):
     assert StringChecker.has_number(self.s1) == False
-    assert StringChecker.has_number(self.s2) == True
     assert StringChecker.has_number(self.s3) == True
     assert StringChecker.has_number(self.s4) == True
     assert StringChecker.has_number(self.s5) == False
-    assert StringChecker.has_number(self.s6) == False
-    assert StringChecker.has_number(self.s7) == True
+    assert StringChecker.has_number(self.s7) == False
+    assert StringChecker.has_number(self.s8) == True
 
   def test_has_word(self):
-    assert StringChecker.has_letter(self.s1) == True
-    assert StringChecker.has_letter(self.s2) == False
-    assert StringChecker.has_letter(self.s3) == True
+    assert StringChecker.has_letter(self.s2) == True
+    assert StringChecker.has_letter(self.s3) == False
     assert StringChecker.has_letter(self.s4) == True
     assert StringChecker.has_letter(self.s5) == True
     assert StringChecker.has_letter(self.s6) == False
-    assert StringChecker.has_letter(self.s7) == True
+    assert StringChecker.has_letter(self.s7) == False
 
   def test_includes(self):
     assert StringChecker.includes(self.s1, "Hello") == True
-    assert StringChecker.includes(self.s2, "123") == True
-    assert StringChecker.includes(self.s3, "abc") == True
-    assert StringChecker.includes(self.s4, "1 2") == True
-    assert StringChecker.includes(self.s5, "NoNumbers") == True
-    assert StringChecker.includes(self.s6, "test") == False
-    assert StringChecker.includes(self.s7, "789") == False
+    assert StringChecker.includes(self.s3, "123") == False
+    assert StringChecker.includes(self.s4, "!0") == True
+    assert StringChecker.includes(self.s6, "!@#$%^&*()") == True
+    assert StringChecker.includes(self.s6, "!@#$%^&*() ") == False
+    assert StringChecker.includes(self.s7, "") == True
+    assert StringChecker.includes(self.s8, "") == True
 
-class TestCourseChecker(unittest.TestCase):
+class TestCourseChecker(TestCourse):
   """
-  Test module course_checker
+  Test module course_checker.
   """
-  def setUp(self):
-    self.course1 = Course(
-      uid_='1', code_='CSC101', subject_='Computer Science', number_='101',
-      name_='Introduction to Computer Science', longname_='CS 101',
-      info_='Introductory course covering basic concepts in computer science.',
-      prereq_=None
-    )
-    self.course2 = Course(
-      uid_='2', code_='CSC101', subject_='Computer Science', number_='101',
-      name_='Introduction to Computer Science', longname_='CS 101',
-      info_='Introductory course covering basic concepts in computer science.',
-      prereq_=None
-    )
-    self.course3 = Course(
-      uid_='3', code_='CSC102', subject_='Computer Science', number_='102',
-      name_='Data Structures', longname_='CS 102',
-      info_='A course focusing on data structures and algorithms.',
-      prereq_=None
-    )
-    self.course_shell1 = CourseShell(uid_='1', code_='CSC101', subject_='Computer Science', number_='101')
-    self.course_shell2 = CourseShell(uid_='2', code_='CSC101', subject_='Computer Science', number_='101')
-    self.course_shell3 = CourseShell(uid_='3', code_='CSC102', subject_='Computer Science', number_='102')
-
-  def tearDown(self):
-    pass
-
   def test_is_equal(self):
     # Test for Course type
-    assert CourseChecker.is_equal(self.course1, self.course1) == True
-    assert CourseChecker.is_equal(self.course1, self.course2) == False
-    assert CourseChecker.is_equal(self.course1, self.course3) == False
+    assert CourseChecker.is_equal(self.courses[0], self.courses[0]) == True
+    assert CourseChecker.is_equal(self.courses[0], self.courses[1]) == False
+    assert CourseChecker.is_equal(self.courses[0], self.courses[2]) == False
 
     # Test for CourseShell type
-    assert CourseChecker.is_equal(self.course_shell1, self.course_shell1) == True
-    assert CourseChecker.is_equal(self.course_shell1, self.course_shell2) == False
-    assert CourseChecker.is_equal(self.course_shell1, self.course_shell3) == False
+    assert CourseChecker.is_equal(self.course_shells[0], self.course_shells[0]) == True
+    assert CourseChecker.is_equal(self.course_shells[0], self.course_shells[1]) == False
+    assert CourseChecker.is_equal(self.course_shells[0], self.course_shells[2]) == False
 
     # Test for objects of different types
-    assert CourseChecker.is_equal(self.course1, self.course_shell1) == False
+    assert CourseChecker.is_equal(self.courses[0], self.course_shells[0]) == False
 
-class TestFilter(unittest.TestCase):
-  """
-  Test module filter
-  """
-  def setUp(self):
-    pass
-  
-  def tearDown(self):
-    pass
-
-  pass
-
-class TestStringFilter(unittest.TestCase):
+class TestStringFilter(TestString):
   """
   Test cases for module string_filter
   """
-  def setUp(self):
-    pass
-  
-  def tearDown(self):
-    pass
+  pass
 
   def test_string_filter_space(self):
     pass
@@ -284,29 +316,21 @@ class TestStringFilter(unittest.TestCase):
   def test_string_filter_redundancy(self):
     pass
 
-class TestCourseFilter(unittest.TestCase):
+class TestCourseFilter(TestCourse):
   """
-  Test module course_filter
+  Test module course_filter.
   """
-  def setUp(self):
-    pass
-  
-  def tearDown(self):
-    pass
-
   pass
 
 class TestPrereqFilter(unittest.TestCase):
   """
-  Test module prereq_filter
+  Test module prereq_filter.
   """
   def setUp(self):
     pass
   
   def tearDown(self):
     pass
-
-  pass
 
 # Run all tests by default
 if __name__ == '__main__':
