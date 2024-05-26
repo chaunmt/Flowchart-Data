40f5be3471f3f779eaee68f8af815c37e072c975
Chau Truong <chauminh.0210@gmail.com>
2024-05-25 20:56:46 -0500

    move doc string down to method to make it show on hover

--- src/python/Filter/string_filter.py
+++ src/python/Filter/string_filter.py
@@ -7,34 +7,41 @@ class StringFilter(Filter):
   allowed_type = str
 
 class StringFilterSpace(StringFilter):
-  """
-  Delete all spaces in string component
-  and return the new string.
-  """
-
   def process(self):
+    """
+    Delete all spaces in string component
+    and return the new string.
+    """
     return self.item.replace(' ', '')
 
 class StringFilterSigns(StringFilter):
-  """
-  Replace certain signs with corresponding words
-  and return the new string.
-  """
-
   def process(self):
+    """
+    Replace certain signs with corresponding words
+    and return the new string.
+    """
     item_ = self.item
     item_ = item_.replace(",", " and ")
     item_ = item_.replace("/", " or ")
     item_ = item_.replace("&", " and ")
     return item_
 
-class StringFilterRedundancy(StringFilter):
-  """
-  Delete all redundant substrings
-  and return the new string.
-  """
-
+class StringFilterBrackets(StringFilter):
   def process(self):
+    """
+    Replace parentheses with square brackets.
+    """
+    item_ = self.item
+    item_ = item_.replace("(", "[")
+    item_ = item_.replace(")", "]")
+    return item_
+
+class StringFilterRedundancy(StringFilter):
+  def process(self):
+    """
+    Delete all redundant substrings
+    and return the new string.
+    """
     item_ = self.item
     item_ = HelpSplit.StringSplitter.at_last_type_occurrence(
       item_, 'number'
