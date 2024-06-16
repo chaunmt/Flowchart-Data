e67855f2e5c85a93ad6454d44ea165870a92c43a
Chau Truong <chauminh.0210@gmail.com>
2024-06-15 22:29:06 -0500

    lower coupling and remove circular import

--- src/python/Helper/course_info_splitter.py
+++ src/python/Helper/course_info_splitter.py
@@ -6,8 +6,7 @@ split a course's info string into a list of strings.
 import re
 
 from python.helper.string_splitter import StringSplitter
-from python.helper.checker.string_checker import StringChecker
-from python.converter.prereq_logic_converter import PrereqLogicConverter
+from python.helper.checker.course_checker import CourseChecker
 from python.filter.string_filter import StringFilterSpace
 
 class CourseInfoSplitter:
@@ -17,86 +16,111 @@ class CourseInfoSplitter:
 
     #############################################################################
     @classmethod
-    def code_into_subj_num_suf(cls, s: str) -> list:
+    def to_subj_num_suf(cls, s: str) -> list:
         """
         Split a string of Course's code into a list of [subject, number, suffix].
         """
 
-        # Remove all spaces
-        s = StringFilterSpace(s)
-        s = s.process()
+        splits = StringSplitter.letter_and_num(s.upper())
 
-        if (
-            StringChecker.has_sign(s) or   # code does not have sign
-            StringChecker.is_empty(s) or    # code can't be empty
-            not StringChecker.has_number(s) # code has to have number
-        ):
+        subject, number, suffix = [None, None, None]
+        for split in splits:
+            # If exists, subject has to come before number
+            if split.isalpha() and not subject and not number:
+                if CourseChecker.is_valid_subj(split):
+                    subject = split
+                else:
+                    subject = ''
+
+            # Number can exist by itself and a code has to have a number
+            elif split.isdigit() and not number:
+                if CourseChecker.is_valid_num(split):
+                    number = split
+                else:
+                    return [None, None, None]
+
+            # If exists, suffix has to be the last member
+            elif number and not suffix:
+                if CourseChecker.is_valid_suf(split):
+                    suffix = split
+                else:
+                    suffix = ''
+                break
+
+        # Number is required for valid code
+        if not number:
             return [None, None, None]
-
-        subject, number = StringSplitter.at_first_type_occurrence(s, 'number')
-
-        if StringChecker.is_empty(subject):
-            subject = None
-
-        number, suffix = cls.separate_num_suf(number)
+        
+        if not subject:
+            subject = ''
+            
+        if not suffix:
+            suffix = ''
 
         return [subject, number, suffix]
 
     #############################################################################
     @staticmethod
-    def separate_num_suf(s: str) -> list:
+    def to_num_suf(s: str) -> list:
         """
         Split a string of Course's number with suffix into a list of [number, suffix].
         """
 
-        # Remove all spaces
-        s = StringFilterSpace(s)
-        s = s.process()
+        splits = StringSplitter.letter_and_num(s.upper())
 
-        if (
-            StringChecker.has_sign(s) or   # code does not have sign
-            StringChecker.is_empty(s) or    # code can't be empty
-            not StringChecker.has_number(s) # code has to have number
-        ):
+        number, suffix = [None, None]
+        for split in splits:
+            # Number can exist by itself
+            if split.isdigit() and not number:
+                if CourseChecker.is_valid_num(split):
+                    number = split
+                else:
+                    return [None, None]
+
+            # If exists, suffix has to be the last member
+            elif number and not suffix:
+                if CourseChecker.is_valid_suf(split):
+                    suffix = split
+                else:
+                    suffix = ''
+                break
+
+        # Number is required for valid value
+        if not number:
             return [None, None]
-
-        number, suffix = StringSplitter.at_last_type_occurrence(s, 'number')
-
-        if StringChecker.is_empty(suffix):
-            suffix = None
-
+        
+        if not suffix:
+            suffix = ''
+        
         return [number, suffix]
 
     #############################################################################
     @staticmethod
-    def get_course_codes(s: str, target_course_subject: str) -> list:
+    def to_codes(s: str) -> list:
         """
-        Get a list of course's code out of the original string.\n
+        Get a list of substring with course's code's format out of the original string.\n
         Noted: Encoded key's prefix "NESTEDSTR" is treated as a subject,
         making the complete key a valid course's code.\n
 
-        EX: "Students have to take CSCI 2041 and 2021 or NESTEDSTR0."\n
-        ==> [ "CSCI2041", "CSCI2021", "NESTEDSTR0" ]\n
+        EX: "Students have to take CSCI 2041 and 2021 or NESTEDSTR0, 3081W."\n
+        ==> [ "CSCI2041", "AND2021", "NESTEDSTR0", "3081W" ]\n
         """
 
         # The regex pattern to get out acceptable code or partial code
         pattern = (
             '\b[A-Za-z]+\\s?\\d{2,4}[A-Za-z]*\b'  # Full code pattern (EX: CSCI 3081W)
-            + '\b\\d{2,4}[A-Za-z]*\b'  # No subject code pattern (EX: 3081W)
+            + '\b\\d{4}[A-Za-z]{0,1}*\b'  # No subject code pattern (EX: 3081W, 4041)
             + '\bNESTEDSTR\\d+\b'  # Encoded key pattern (EX: NESTEDSTR0)
         )
 
         # Get the list of matches strings
         course_codes = re.findall(pattern, s)
 
-        # Convert all partial codes into full code format
-        course_codes = PrereqLogicConverter.missing_subject_converter(
-            course_codes,
-            target_course_subject
-        )
-
-        # Remove all space to follow CourseDog's course code format
         for index, code in enumerate(course_codes):
+            # Remove all space to follow CourseDog's course code format
             course_codes[index] = StringFilterSpace(code).process()
+
+            # Convert all letters to uppercase
+            course_codes[index] = course_codes[index].upper()
 
         return course_codes
