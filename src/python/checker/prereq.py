42c24c1b89c38e89a7370217ce4e5e38216f2e2f
Chau Truong <chauminh.0210@gmail.com>
2024-08-12 21:49:51 -0500

    refactor prereq checker

--- src/python/checker/prereq.py
+++ src/python/checker/prereq.py
@@ -1,12 +1,94 @@
 """
-This module contains class CourseChecker which
-perform checks on Course type related object.
+This module contains checkers on prerequisites object .\n
+It includes CourseChecker and CourseInfoChecker.
 """
 
 from python.schema.prereq import Course, CourseShell
+from python.splitter.prereq import CourseInfoSplitter
 from python.sources import JSONHandler
 
-class CourseChecker:
+class CourseInfoChecker:
+    """
+    Perform checks on an info string of Course's type object.
+    """
+
+    #############################################################################
+    @staticmethod
+    def is_writing_suf(suffix: str) -> bool:
+        """
+        Check whether a Course's suffix is of writing types.
+        """
+
+        if suffix in ('W', 'V'):
+            return True
+        return False
+
+    #############################################################################
+    @staticmethod
+    def is_honors_suf(suffix: str) -> bool:
+        """
+        Check whether a Course's suffix is of honors types.
+        """
+
+        if suffix in ('H', 'V'):
+            return True
+        return False
+
+    #############################################################################
+    @staticmethod
+    def is_valid_subj(subject: str) -> bool:
+        """
+        Check whether a Course's subject is valid.
+        """
+
+        # Get JSON data for valid subject codes
+        path = '../data/UMNTC/allSubjects.json'
+        data = JSONHandler.get_from_path(path)
+
+        # Only department code is a valid subject code
+        if subject in data:
+            return True
+
+        return False
+
+    #############################################################################
+    @staticmethod
+    def is_valid_num(number: str) -> bool:
+        """
+        Check whether a Course's number is valid.\n
+        Note: Pre-college courses (Course with its 'number' value < 1000)
+        are excluded from our data (check Readme file for information).
+        """
+
+        # NoneType is not a valid number
+        if not number:
+            return False
+
+        # Remove '0' prefix
+        number = number.lstrip('0')
+
+        return number.isdigit() and len(number) == 4
+
+    #############################################################################
+    @staticmethod
+    def is_valid_suf(suffix: str) -> bool:
+        """
+        Check whether a Course's suffix is valid.
+        """
+
+        if suffix in ['', 'W', 'H', 'V']:
+            return True
+
+        return False
+
+    #############################################################################
+    @staticmethod
+    def is_valid_code(suffix: str) -> bool:
+        """
+        Check whether a Course's code is valid.
+        """
+
+class CourseChecker(CourseInfoChecker):
     """
     Perform checks on Course type related object.
     """
@@ -30,70 +112,3 @@ class CourseChecker:
             return False
 
         return True
-
-    #############################################################################
-    @staticmethod
-    def is_writing(suffix: str) -> bool:
-        """
-        Check whether a course is a writing course based on its code's suffix.
-        """
-
-        if suffix in ('W', 'V'):
-            return True
-        return False
-
-    #############################################################################
-    @staticmethod
-    def is_honors(suffix: str) -> bool:
-        """
-        Check whether a course is an honors course based on its code's suffix.
-        """
-
-        if suffix in ('H', 'V'):
-            return True
-        return False
-
-    #############################################################################
-    @staticmethod
-    def is_valid_subj(subject: str) -> bool:
-        """
-        Check whether a course's subject is valid.
-        """
-
-        # Get JSON data for valid subject codes
-        path = '../data/UMNTC/allSubjects.json'
-        data = JSONHandler.get_from_path(path)
-
-        # Only department code is a valid subject code
-        if subject in data:
-            return True
-
-        return False
-
-    #############################################################################
-    @staticmethod
-    def is_valid_num(number: str) -> bool:
-        """
-        Check whether a course's number is valid.
-        """
-
-        # NoneType is not a valid number
-        if not number:
-            return False
-
-        # Remove '0' prefix
-        number = number.lstrip('0')
-
-        return number.isdigit() and len(number) == 4
-
-    #############################################################################
-    @staticmethod
-    def is_valid_suf(suffix: str) -> bool:
-        """
-        Check whether a course's suffix is valid.
-        """
-
-        if suffix in ['', 'W', 'H', 'V']:
-            return True
-
-        return False
