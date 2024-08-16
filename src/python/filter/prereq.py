1de0b70570fafb7fb366581e5485c15846eb333f
Chau Truong <chauminh.0210@gmail.com>
2024-08-15 23:41:17 -0500

    add PrereqFilterNonUid

--- src/python/filter/prereq.py
+++ src/python/filter/prereq.py
@@ -5,6 +5,7 @@ filter prereq component and return its new result.
 
 from python.filter.filter import Filter
 from python.schema.prereq import PrereqFormat
+from python.checker.string import StringChecker
 
 ###############################################################################
 class PrereqFilter(Filter):
@@ -168,6 +169,68 @@ class PrereqFilterDuplicate(PrereqFilter):
                 res = {}
                 for key, value in prereq.items():
                     if value not in res.items():
+                        res[key] = value
+                prereq = res
+
+                changed = False
+                for key, value in prereq.items():
+                    # Recursively filter nested value
+                    new_prereq = cls.filter(value)
+
+                    if prereq[key] != new_prereq:
+                        changed = True
+                        prereq[key] = new_prereq
+
+                # If no value is changed, all values are filtered
+                if not changed:
+                    break
+
+        return prereq
+
+###############################################################################
+class PrereqFilterNonUid(PrereqFilter):
+    """
+    This class filter out all elements that are not in the form of a course's uid.\n
+    """
+
+    def filter(cls):
+        """
+        Delete all elements that are not in course's uid form
+        and return the new prereq.
+        """
+
+        prereq = cls.item
+
+        if isinstance(prereq, list):
+            # Filter all possible elements
+            while True:
+                # Delete all non-uid elements from list
+                res = []
+                for value in prereq:
+                    if value.isdigit():
+                        res.append(value)
+                prereq = res
+
+                changed = False
+                for index, value in enumerate(prereq):
+                    # Recursively filter nested value
+                    new_prereq = cls.filter(value)
+
+                    if prereq[index] != new_prereq:
+                        changed = True
+                        prereq[index] = new_prereq
+
+                # If no value is changed, all values are filtered
+                if not changed:
+                    break
+
+        elif isinstance(prereq, dict): # Logical operation 'and', 'or' and their value
+            # Filter all possible elements
+            while True:
+                # Delete all non-uid elements from dictionary
+                res = {}
+                for key, value in prereq.items():
+                    if value.isdigit():
                         res[key] = value
                 prereq = res
 
