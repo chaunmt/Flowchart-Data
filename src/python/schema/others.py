385e0196946763233bd67c40a19e3999178ac73c
Chau Truong <chauminh.0210@gmail.com>
2024-09-07 07:21:10 -0500

    add StringComponent for Decorator (Filter)

---
+++ src/python/schema/others.py
@@ -0,0 +1,24 @@
+"""
+This class contains multiple classes to help define new types for decorators (filter etc.).
+"""
+
+class StringComponent:
+    """
+    A string component class consists of a private string variable
+    that can be wrapped in decorators like StringFilter's children class.
+    """
+    
+    #####################################
+    def __init__(
+            self,
+            s: str
+        ) -> None:
+        """
+        Initialize a string object.
+        """
+
+        self._s = s
+
+    #####################################
+    def process(self) -> dict:
+        return self._s
