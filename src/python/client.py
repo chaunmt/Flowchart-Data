52c2093f8835d422b2485d63938fba9e586e784a
Chau T. <chauminh.0210@gmail.com>
2024-10-21 22:50:46 -0500

    fix typo

--- src/python/client.py
+++ src/python/client.py
@@ -2,9 +2,9 @@
 This is what the highest level where client would see.
 """
 
-# from python.sources.api.facade import CourseDogFacade
-# from python.sources.api.coursedog.interface import CourseSystemInterface, ProgramSystemInterface
+from python.sources.api.facade import CourseDogFacade
+from python.sources.api.coursedog.interface import CourseSystemInterface, ProgramSystemInterface
 
-# facade = CourseDogFacade(CourseSystemInterface(), ProgramSystemInterface())
+facade = CourseDogFacade(CourseSystemInterface(), ProgramSystemInterface())
 
-# print(facade.operation())
+print(facade.operation())
